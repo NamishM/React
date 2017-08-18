4f090bbc34bee702e2d6c32a9e07374474ee2033
nmudgal <namish_mudgal@rediffmail.com>
2017-08-18 20:28:32 +0530

    footer commit 1

--- Project2/src/modules/Footer/components/Footer.jsx
+++ Project2/src/modules/Footer/components/Footer.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import Headroom from 'react-headroom';
 import '../css/Footer.css';
 
 const Footer = () => (
-  <footer class="container_wrap socket_color">
-    <div class="container">
-      <span class="copyright">© Copyright  - <a href="/">Namish One Page Portfolio </a> </span>
-      <ul class="noLightbox social_bookmarks icon_count_3">
-        <li class="social_bookmarks_twitter av-social-link-twitter social_icon_1"><a target="_blank" href="http://twitter.com/" aria-hidden="true" data-av_icon="" data-av_iconfont="entypo-fontello" title="Twitter"><span class="avia_hidden_link_text">Twitter</span></a></li>
-        <li class="social_bookmarks_facebook av-social-link-facebook social_icon_2"><a target="_blank" href="http://facebook.com/" aria-hidden="true" data-av_icon="" data-av_iconfont="entypo-fontello" title="Facebook"><span class="avia_hidden_link_text">Facebook</span></a></li>
-        <li class="social_bookmarks_dribbble av-social-link-dribbble social_icon_3"><a target="_blank" href="http://dribbble.com/" aria-hidden="true" data-av_icon="" data-av_iconfont="entypo-fontello" title="Dribbble"><span class="avia_hidden_link_text">Dribbble</span></a></li>
+  <footer
+    className="footer"
+    style={{ background: '#2b2b2b', height: '50px' }}
+  >
+    <div className="container">
+      <span className="copyright">© Copyright 2017 - <a href="/">Namish One Page Portfolio </a> </span>
+      <ul className="social_bookmarks">
+        <li className="twitter"><a target="_blank" href="http://twitter.com/" rel="noopener noreferrer" aria-hidden="true" title="twitter"><span className="hidden_link_text">Twitter</span></a></li>
+        <li className="facebook"><a target="_blank" href="http://facebook.com/" rel="noopener noreferrer" aria-hidden="true" title="Facebook"><span className="hidden_link_text">Facebook</span></a></li>
+        <li className="dribbble"><a target="_blank" href="http://dribbble.com/" rel="noopener noreferrer" aria-hidden="true" title="Dribbble"><span className="hidden_link_text">Dribbble</span></a></li>
       </ul>
     </div>
   </footer>
--- Project2/src/modules/common/components/MainView.jsx
+++ Project2/src/modules/common/components/MainView.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from 'modules/Header/components/Header';
+import Footer from 'modules/Footer/components/Footer';
 
 const MainView = () => (
   <div>
     <Header />
     <iframe id="iframe" src="./assets/saved_resource.html" title="iframe" height="709px" />
+    <Footer />
   </div>
 );
 
